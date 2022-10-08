7f3133b53f66ecef49cb03949fda85f5117ed07f
tanka <tanka.f05@gmail.com>
2022-10-08 22:19:28 +0300

    minMax function

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,16 @@
-function getMinMax(str) {
+function getMinMax(inputData ) {
   // ваш код...
+  let myArr = inputData.split(' ');
+
+  let minValue = myArr[0];
+
+  let maxValue = minValue;
+
+  for (let i = 0; i < myArr.length; i++) {
+    let el = +myArr[i];
+    if (el < minValue) minValue = el;
+    else if (el > maxValue) maxValue = el;
+  }
+
+  return {min: minValue, max: maxValue};
 }

752a00fa84cc77ac13e53b2c8a9e8a5507e5cfc0
tanka <tanka.f05@gmail.com>
2022-10-12 22:46:52 +0300

    highlight function

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,27 @@
 function highlight(table) {
   // ваш код...
+  for (let row of table.children[1].rows) {
+    //available
+    let dataAvailable = row.cells[3];
+
+    if (dataAvailable.hasAttribute("data-available")) {
+      if (dataAvailable.dataset.available === "true") {
+        row.classList.add("available");
+      } else row.classList.add("unavailable");
+    } else row.hidden = true;
+
+
+    //gender
+    let gender = row.cells[2].textContent;
+    if (gender === 'm') {
+      row.classList.add("male");
+    } else if (gender === 'f') row.classList.add("female");
+
+
+    //age
+    let age = row.cells[1].textContent;
+    if (age < 18) {
+      row.style.textDecoration = 'line-through';
+    }
+  }
 }

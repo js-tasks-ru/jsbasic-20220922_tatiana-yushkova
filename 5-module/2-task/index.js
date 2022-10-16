05e37352289c86caf4ec2e1850061f8e4c355b91
tanka <tanka.f05@gmail.com>
2022-10-16 22:54:34 +0300

    toggleText function

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,11 @@
 function toggleText() {
   // ваш код...
+  let myButton = document.querySelector('.toggle-text-button');
+  let textBlock = document.querySelector('#text');
+
+  myButton.addEventListener('click', toggleText);
+
+  function toggleText() {
+    textBlock.toggleAttribute('hidden');
+  }
 }

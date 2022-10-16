31d9d777c9146f980da8718222ecff67e202361b
tanka <tanka.f05@gmail.com>
2022-10-16 22:58:37 +0300

    hideSelf function

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,10 @@
 function hideSelf() {
   // ваш код...
+  let myButton = document.querySelector('button');
+
+  myButton.addEventListener('click', hideSelf);
+
+  function hideSelf() {
+      return myButton.setAttribute("hidden", true);
+  }
 }
